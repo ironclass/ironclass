d8e664ac6f3fc06ed68548188dee1c91e6bdc8a4
Malte <m.felmy@googlemail.com>
2019-02-12 18:18:40 +0100

    Seperate script.js and setDate.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -30,19 +30,3 @@ socket.on("dequeueStudent", () => {
   let ul = document.getElementById("call-list");
   ul.children[0].remove()
 });
-
-// set current date as max value for all date input forms
-
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-if (dd < 10) {
-  dd = "0" + dd;
-}
-if (mm < 10) {
-  mm = "0" + mm;
-}
-
-today = yyyy + "-" + mm + "-" + dd;
-document.getElementById("datefield").setAttribute("max", today);
