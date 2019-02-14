60801468c6109e40088da96ea29ec11287184197
André Sebastian <a.sebastian@posteo.de>
2019-02-14 17:58:52 +0100

    create index.html for testing purposes

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ const {dynamicSort}  = require("../src/helpers");
 
 // L O G I N
 router.get("/login", (req, res, next) => {
+  // res.send("/views/landing.html")
   res.render("auth/login", { message: req.flash("error") });
 });
 
