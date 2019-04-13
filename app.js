1213af0fbffb193f402541c2f11ec68309b28670
André Sebastian <a.sebastian@posteo.de>
2019-04-13 14:29:22 +0200

    not show people in queue in title, when it is empty

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ require("./passport")(app);
 app.use((req, res, next) => {
   res.locals.isConnected = !!req.user;
   res.locals.isNotConnected = !req.user;
-
+  
   res.locals.error  = req.flash("error");
   res.locals.success  = req.flash("success");
  
